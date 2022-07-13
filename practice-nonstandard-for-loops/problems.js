1dcea304d4347766a2c200f5d03142ce6fc79b2e
David Kim <dkhw90@gmail.com>
2022-07-13 21:30:19 +0200

    Finish five questions from non standard loops

---
+++ practice-nonstandard-for-loops/problems.js
@@ -0,0 +1,65 @@
+function oddIndices(arr) {
+    // Return an array containing all the odd indices in the array
+    // Your code here
+    let oddIdxs = []
+
+    for (let idx = 1; idx < arr.length; idx += 2) oddIdxs.push(arr[idx])
+    return oddIdxs;
+}
+
+function oddReverse(arr) {
+    // Return an array containing all the odd indices starting from the end
+    // Your code here
+    let oddIdxs = []
+
+    for (let idx = 1; idx < arr.length; idx += 2) oddIdxs.push(arr[idx])
+    return oddIdxs.reverse();
+}
+
+function secondPower(arr) {
+    // Return an array containing all indices that are powers of 2
+    // Your code here
+    function isPowerofTwo(v) {
+        return v && !(v & (v - 1));
+    }
+
+    let powerOfTwo = []
+
+    return arr.filter((idx, el) => isPowerofTwo(el));
+}
+
+function nthPower(arr, n) {
+    // Return an array containing all indices that are powers of n
+    // Your code here
+    function powerOfN(x) {
+        return (Math.log(x)/Math.log(n)) % 1 === 0;
+    }
+
+    return arr.filter((idx, el) => powerOfN(el));
+}
+
+function firstHalf(arr) {
+    // Return an array containing the first half of an array
+    // Include middle index on odd length arr
+    // Your code here
+
+    let mid = Math.floor(arr.length / 2);
+    return arr.length % 2 !== 0 ? arr.slice(0, mid + 1) : arr.slice(0, mid);
+}
+
+function secondHalf(arr) {
+    // Return an array containing the second half of an array
+    // Exclude middle index on odd length arr
+    // Your code here
+    let mid = Math.floor(arr.length / 2);
+    return arr.length % 2 !== 0 ? arr.slice(mid + 1) : arr.slice(mid);
+}
+
+module.exports = {
+    oddIndices,
+    oddReverse,
+    secondPower,
+    nthPower,
+    firstHalf,
+    secondHalf
+}
