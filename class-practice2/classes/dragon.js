44325c2121894c3287634488ab7aa34adf5ba8c6
David Kim <dkhw90@gmail.com>
2022-12-02 20:25:09 +0100

    Wrap up final part of classes practice project

--- class-practice2/classes/dragon.js
+++ class-practice2/classes/dragon.js
@@ -1,4 +1,27 @@
 // Your code here
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+
+  breathesFire() {
+    return `${this.name} breathes fire everywhere! BURN!!!!`;
+  }
+
+  static getDragons(...dragons) {
+    let result = []
+    for (let dragon of dragons) {
+      result.push(dragon.name);
+    }
+    return result;
+  }
+}
+
+
+const puff = new Dragon("Puff", "green");
+const toothless = new Dragon("Toothless", "black");
+console.log(Dragon.getDragons(puff, toothless)); // [ 'Puff', 'Toothless' ]
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
