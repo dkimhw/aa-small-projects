6bbbd0ed9f8d9c75ad33616b67555c56cb09b79e
David Kim <dkhw90@gmail.com>
2022-12-23 23:32:22 +0100

    Add cursor movement methods

---
+++ tic-tac-toe/class/ttt.js
@@ -0,0 +1,52 @@
+const Screen = require("./screen");
+const Cursor = require("./cursor");
+
+class TTT {
+
+  constructor() {
+
+    this.playerTurn = "O";
+
+    this.grid = [[' ',' ',' '],
+                 [' ',' ',' '],
+                 [' ',' ',' ']]
+
+    this.cursor = new Cursor(3, 3);
+
+    // Initialize a 3x3 tic-tac-toe grid
+    Screen.initialize(3, 3);
+    Screen.setGridlines(true);
+
+    // Replace this with real commands
+    Screen.addCommand('up', 'Move cursor up', this.cursor.up.bind(this.cursor));
+    Screen.addCommand('down', 'Move cursor down', this.cursor.down.bind(this.cursor));
+    Screen.addCommand('right', 'Move cursor right', this.cursor.right.bind(this.cursor));
+    Screen.addCommand('left', 'Move cursor left', this.cursor.left.bind(this.cursor));
+
+    Screen.render();
+  }
+
+  static checkWin(grid) {
+
+    // Return 'X' if player X wins
+    // Return 'O' if player O wins
+    // Return 'T' if the game is a tie
+    // Return false if the game has not ended
+
+  }
+
+  static endGame(winner) {
+    if (winner === 'O' || winner === 'X') {
+      Screen.setMessage(`Player ${winner} wins!`);
+    } else if (winner === 'T') {
+      Screen.setMessage(`Tie game!`);
+    } else {
+      Screen.setMessage(`Game Over`);
+    }
+    Screen.render();
+    Screen.quit();
+  }
+
+}
+
+module.exports = TTT;
