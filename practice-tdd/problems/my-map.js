3d1467f430cf90011edb1bb2d0ca7f7ab4b97d4a
David Kim <dkhw90@gmail.com>
2023-03-18 02:06:30 +0100

    Practice creating tests using chai spy

--- practice-tdd/problems/my-map.js
+++ practice-tdd/problems/my-map.js
@@ -1,5 +1,17 @@
+
+
 function myMap(inputArray, callback) {
   // Your code here
+  let result = []
+
+  inputArray.forEach((input) => {
+    const returnVal = callback(input);
+    result.push(returnVal);
+  });
+
+  return result;
+
+  // return inputArray.map(callback);
 }
 
 module.exports = myMap;
--- practice-tdd/test/my-map-spec.js
+++ practice-tdd/test/my-map-spec.js
@@ -1,1 +1,32 @@
 // Your code here
+const chai = require("chai");
+const myMap = require("../problems/my-map");
+const expect = chai.expect;
+const spies = require("chai-spies");
+chai.use(spies);
+
+describe("myMap", function() {
+  beforeEach(() => {
+    testArr = new Array(1, 2, 3, 4, 5);
+    cb = function (el) {
+      el * 2
+    };
+  });
+
+  it ("should not mutate input array", function() {
+    myMap(testArr, cb);
+    expect(testArr).to.eql([1, 2, 3, 4, 5])
+  });
+
+  it ("should not use built-in method Array.map", function() {
+    let spy = chai.spy.on(testArr, 'map');
+    myMap(testArr, cb);
+    expect(spy).to.not.have.been.called.exactly(1);
+  });
+
+  it ("should call cb on each element", function() {
+    let spy = chai.spy(cb);
+    myMap(testArr, spy);
+    expect(spy).to.have.been.called.exactly(testArr.length);
+  })
+})
