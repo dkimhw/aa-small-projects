b69f6d6d8cc6d96bfac654c8dd1dc530f4a1070b
David Kim <dkhw90@gmail.com>
2023-03-19 02:06:54 +0100

    Create test spec for Person class

--- practice-tdd/problems/person.js
+++ practice-tdd/problems/person.js
@@ -1,5 +1,38 @@
 class Person {
   // Your code here
+  constructor (name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  sayHello() {
+    return `Welcome ${this.name}`;
+  }
+
+  visit(otherPerson) {
+    return `${this.name} visited ${otherPerson.name}`;
+  }
+
+  switchVisit(otherPerson) {
+    return otherPerson.visit(this);
+  }
+
+  update(obj) {
+    if (typeof obj !== 'object') {
+      throw new TypeError('Must be an object');
+    }
+
+    if (!obj.hasOwnProperty('name') || !obj.hasOwnProperty('age')) {
+
+      console.log("hello")
+      throw new TypeError('Must contain name and age properties');
+    }
+
+    this.name = obj.name;
+    this.age = obj.age;
+
+    return this;
+  }
 }
 
 module.exports = Person;
--- practice-tdd/test/person-spec.js
+++ practice-tdd/test/person-spec.js
@@ -1,1 +1,45 @@
 // Your code here
+const chai = require("chai");
+const Person = require("../problems/person");
+const expect = chai.expect;
+
+
+describe('Person', function() {
+  beforeEach(() => {
+    testPerson = new Person('David', 33);
+    otherPerson = new Person('Patri', 36)
+  });
+
+  it('should create an instance of a Person', function() {
+    expect(testPerson).to.be.an.instanceof(Person);
+  });
+
+  it('should have properties age and name', function() {
+    expect(testPerson).to.have.property('age', 33);
+    expect(testPerson).to.have.property('name', 'David');
+  });
+
+  it('should return a welcome message with name', function() {
+    expect(testPerson.sayHello()).to.equal('Welcome David');
+  });
+
+  it('should return a visited message', function() {
+    expect(testPerson.visit(otherPerson)).to.equal('David visited Patri');
+  });
+
+  it('should return a other person\'s visited message', function() {
+    expect(testPerson.switchVisit(otherPerson)).to.equal('Patri visited David');
+  });
+
+  it ('should update the name and age with given obj', function() {
+    testPerson.update({ name: "lulu", age: 57 });
+    expect(testPerson).to.have.property('age', 57);
+    expect(testPerson).to.have.property('name', 'lulu');
+  });
+
+  it ('should throw an error', function () {
+    expect(() => testPerson.update({ name: "lulu" })).to.throw(Error);
+  });
+
+
+});
