913b9b15a56f82f2c2334d3f641a53f41aa26d6c
David Kim <dkhw90@gmail.com>
2023-06-14 17:04:57 +0200

    Test execution time of linked list & doubly linked list

--- linked-list-practice/linked-list-timing.js
+++ linked-list-practice/linked-list-timing.js
@@ -7,3 +7,45 @@ Construct a timing test to verify the time complexities of `addToHead` and
 */
 
 // Your code here
+const startLinkedList = Date.now();
+
+let linkedList = new LinkedList();
+
+linkedList.addToHead(1);
+linkedList.addToHead(2);
+linkedList.addToHead(3);
+linkedList.addToHead(4);
+linkedList.addToHead(10);
+linkedList.addToHead(12);
+linkedList.addToHead(15);
+linkedList.addToHead(17);
+linkedList.addToHead(20);
+linkedList.addToHead(25);
+linkedList.addToTail(4);
+linkedList.addToTail(5);
+
+const endLinkedList = Date.now();
+
+console.log(`Execution time: ${endLinkedList - startLinkedList} ms`);
+
+
+const startDoublyLinkedList = Date.now();
+
+let doublyLinkedList = new DoublyLinkedList();
+
+doublyLinkedList.addToHead(1);
+doublyLinkedList.addToHead(2);
+doublyLinkedList.addToHead(3);
+doublyLinkedList.addToHead(4);
+doublyLinkedList.addToHead(10);
+doublyLinkedList.addToHead(12);
+doublyLinkedList.addToHead(15);
+doublyLinkedList.addToHead(17);
+doublyLinkedList.addToHead(20);
+doublyLinkedList.addToHead(25);
+doublyLinkedList.addToTail(4);
+doublyLinkedList.addToTail(5);
+
+const endDoublyLinkedList = Date.now();
+
+console.log(`Execution time: ${endDoublyLinkedList - startDoublyLinkedList} ms`);
